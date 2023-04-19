4d367509bebf3b0d9bf924ffc9240d32826023f4
duvan1026 <duvancastro1026@gmail.com>
2023-04-19 10:54:56 -0500

    create entity

--- src/main/java/com/example/springsecuritycifrado/User.java
+++ src/main/java/com/example/springsecuritycifrado/User.java
@@ -1,4 +1,45 @@
 package com.example.springsecuritycifrado;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ob_user")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
