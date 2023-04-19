e774d72afd05799c537769761d7a84779c3da55c
duvan1026 <duvancastro1026@gmail.com>
2023-04-19 10:01:41 -0500

    implement test of the creaty encryption password and comparison of these

--- src/test/java/com/example/springsecuritycifrado/EncryptionTest.java
+++ src/test/java/com/example/springsecuritycifrado/EncryptionTest.java
@@ -1,2 +1,29 @@
-package com.example.springsecuritycifrado;public class EncryptionTest {
+package com.example.springsecuritycifrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncryptionTest {
+
+
+    /**
+     * BCrypt que genera su propia clave de encriptacion( salt ) de 16 bytes
+     * El resultado de cifrar con Bcrypt será un string 60 caracteres:
+     *
+     * $a versión
+     * $10 fuerza (valor de 4 a 31, por defecto 10)
+     * Los 22 siguientes caracteres son el salt generado
+     */
+    @Test
+    void bcrypTest(){
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword =  passwordEncoder.encode("admin"); // se crea contraseña encriptada
+        System.out.println(hashedPassword);
+
+
+        boolean matches = passwordEncoder.matches("admin",hashedPassword); // se compara contraseña encriptada con la que se ingreso
+        System.out.println("la Contraseña coincide? " + matches);
+    }
+
 }
