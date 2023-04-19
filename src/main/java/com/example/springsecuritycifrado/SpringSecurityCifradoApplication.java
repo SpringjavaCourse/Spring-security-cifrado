f48cd189c4aa00b6a41eb441c5e66dc6ebf0a37a
duvan1026 <duvancastro1026@gmail.com>
2023-04-19 11:17:01 -0500

    update file

--- src/main/java/com/example/springsecuritycifrado/SpringSecurityCifradoApplication.java
+++ src/main/java/com/example/springsecuritycifrado/SpringSecurityCifradoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityCifradoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringSecurityCifradoApplication.class, args);
 	}
 
