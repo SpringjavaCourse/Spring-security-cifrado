2e567644072cff4f40e778770b6e06f240f381b3
duvan1026 <duvancastro1026@gmail.com>
2023-04-19 09:10:00 -0500

    add files

---
+++ src/main/java/com/example/springsecuritycifrado/SpringSecurityCifradoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritycifrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCifradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCifradoApplication.class, args);
+	}
+
+}
