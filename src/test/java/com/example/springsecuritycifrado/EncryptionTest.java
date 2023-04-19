ca8528377cf2fbd0dbabdbde72782f2e8c4c0e1c
duvan1026 <duvancastro1026@gmail.com>
2023-04-19 10:29:36 -0500

    implement method pbkdf2 for the encryption password

--- src/test/java/com/example/springsecuritycifrado/EncryptionTest.java
+++ src/test/java/com/example/springsecuritycifrado/EncryptionTest.java
@@ -2,6 +2,7 @@ package com.example.springsecuritycifrado;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 public class EncryptionTest {
 
@@ -26,4 +27,24 @@ public class EncryptionTest {
         System.out.println("la Contraseña coincide? " + matches);
     }
 
+    /**
+     * Genera una 10 veces la clave de encriptacion de 60 caracteres
+     */
+    @Test
+    void bcryptCheckMultiplePasswords(){
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        for(int i = 0; i < 10; i++){
+            System.out.println(passwordEncoder.encode("admin"));
+        }
+
+    }
+
+    @Test
+    void pbkdf2() {
+        Pbkdf2PasswordEncoder passwordEncoder = new Pbkdf2PasswordEncoder("miClaveSecreta" , 4, 2, 60);
+        for(int i = 0; i < 10; i++){
+            System.out.println(passwordEncoder.encode("admin"));
+        }
+    }
 }
